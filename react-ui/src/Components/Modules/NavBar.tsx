1d3c4ebdcd8d675ea3ab5c248ddd6a2195d25f7f
Khoi Vu <kairyvd167@gmail.com>
2025-02-13 19:22:32 -0500

    add navBar.tsx

---
+++ react-ui/src/Components/Modules/NavBar.tsx
@@ -0,0 +1,39 @@
+import { To, useNavigate } from "react-router-dom";
+import Logo from "../../assets/icon.svg";
+
+const NavBar = () => {
+  const navigate = useNavigate();
+  const handleClick = (route: To) => {
+    navigate(route);
+  };
+  return (
+    <nav className=" flex justify-between pl-8 h-12 bg-linear-to-r from-blue-600 to-yellow-400">
+      <div
+        className="flex items-center gap-6 hover:cursor-pointer"
+        onClick={() => handleClick("/")}
+      >
+        <img src={Logo} className="size-8 rounded-full" />
+        <div className="font-grench text-3xl text-yellow-600">Sakila</div>
+      </div>
+      <div className="flex justify-end">
+        {["/Films", "/Customers"].map((route, i) => (
+          <button
+            key={i}
+            className="px-7 duration-500 ease-in-out hover:bg-yellow-600 rounded-3xl hover:cursor-pointer"
+            onClick={() => {
+              if (route === "/Films") {
+                return handleClick("/films");
+              } else if (route === "/Customers") {
+                return handleClick("/customers");
+              }
+            }}
+          >
+            {route.substring(1)}
+          </button>
+        ))}
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
