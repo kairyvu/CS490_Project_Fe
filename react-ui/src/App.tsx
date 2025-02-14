e71331a914dd05ef22fb51f2ebf5135cfa9dd6cb
Khoi Vu <kairyvd167@gmail.com>
2025-02-13 19:21:10 -0500

    add icon and css features

--- react-ui/src/App.tsx
+++ react-ui/src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Landing from "./Components/Pages/Landing.tsx";
+import FilmList from "./Components/Pages/FilmList.tsx";
+import CustomerList from "./Components/Pages/CustomerList.tsx";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/films" element={<FilmList />} />
+          <Route path="/customers" element={<CustomerList />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
