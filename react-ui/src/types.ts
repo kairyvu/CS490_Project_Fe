d78f21af9ccd2deaf26befdd76f4be421564a809
Khoi Vu <kairyvd167@gmail.com>
2025-02-19 02:48:07 -0500

    add actor review feature

--- react-ui/src/types.ts
+++ react-ui/src/types.ts
@@ -4,14 +4,6 @@ export interface Film {
   category: string;
   rental_count: number;
 }
-
-export interface ActorFeedProps {
-  actor_id: number;
-  first_name: string;
-  last_name: string;
-  movie_count: number;
-}
-
 export interface FilmDetails {
   description: string;
   release_year: number;
@@ -19,4 +11,10 @@ export interface FilmDetails {
   length: number;
   rating: string;
   special_features: string;
+}
+export interface Actor {
+  actor_id: number;
+  first_name: string;
+  last_name: string;
+  movie_count: number;
 }
